5337a00d77a3b9459415208833eabcdd891542e7
Hasan AYDIN <haydin@datamarket.com.tr>
2024-10-17 14:58:54 +0300

    Push1

---
+++ hdev_ecommerce/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+namespace EventBus.Base.Events;
+
+public class IntegrationEvent
+{
+    [JsonProperty]
+    public Guid Id { get; private set; }
+    [JsonProperty]
+    public DateTime CratedDate { get; private set; }
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CratedDate = DateTime.Now;
+    }
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime cratedDate)
+    {
+        Id = id;
+        CratedDate = cratedDate;
+    }
+}
---
+++ hdev_ecommerce/BuildingBlocks/EventBus/EventBus.Base/SubManagers/InMemoryEventBusSubscriptionManager.cs
@@ -0,0 +1,84 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.Base.Events;
+
+namespace EventBus.Base.SubManagers;
+
+public class InMemoryEventBusSubscriptionManager : IEventBusSubscriptionManager
+{
+    private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
+    private readonly List<Type> _eventTypes;
+    public event EventHandler<string> OnEventRemoved;
+    public Func<string, string> eventNameGetter;
+    public InMemoryEventBusSubscriptionManager(Func<string, string> eventNameGetter)
+    {
+        _handlers = new Dictionary<string, List<SubscriptionInfo>>();
+        _eventTypes = new List<Type>();
+        this.eventNameGetter = eventNameGetter;
+    }
+
+
+    public bool IsEmpty => !_handlers.Keys.Any();
+    public void Clear() => _handlers.Clear();
+
+    public void AddSubscription<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>
+    {
+        var eventName = GetEventKey<T>();
+        AddSubscription(typeof(TH), eventName);
+        if(_eventTypes.Contains(typeof(T)))
+        {
+            _eventTypes.Add(typeof(T));
+        }
+    }
+    private void AddSubscription(Type handlerType, string eventName)
+    {
+        if(!HasSubscriptionsForEvent(eventName))
+        {
+            _handlers.Add(eventName, new List<SubscriptionInfo>());
+        }
+        if (_handlers[eventName].Any(s=>s.HandlerType == handlerType))
+        {
+            throw new ArgumentException($"Handler type {handlerType.Name} already registered for '{eventName}'", nameof(handlerType));
+        }
+    }
+
+   
+
+    public string GetEventKey<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Type GetEventTypeByName(string eventName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasSubscriptionsForEvent(string eventName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveSubscription<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>
+    {
+        throw new NotImplementedException();
+    }
+}
